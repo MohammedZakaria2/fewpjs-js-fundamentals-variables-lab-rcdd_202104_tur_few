43c52edb56d66222ca0fb6f28073b2cc4a49b98c
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-06-22 02:08:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber'
 let mostProfitableNeighborhood = 'Chelsea'
 let companyCeo = 'Susan Smith'
+=======
+const comapnyName = 'Scuber'
+>>>>>>> fac00cf13e3c49d76e8c607dc24c079ee2e32511
